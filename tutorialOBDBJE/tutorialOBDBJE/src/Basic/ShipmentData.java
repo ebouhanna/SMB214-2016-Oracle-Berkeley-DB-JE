99618c926d4a7a7cc5109760558a5b9d199f43b6
unknown <ebouhanna@HO-IT-DBAMN-PC3.ibl.com.lb>
2015-12-28 11:40:25 +0200

    Creating classes and tutorial DB

---
+++ tutorialOBDBJE/tutorialOBDBJE/src/Basic/ShipmentData.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basic;
+
+/**
+ *
+ * @author lenovo
+ */
+import java.io.Serializable;
+
+public class ShipmentData implements Serializable
+{
+    private int quantity;
+
+    public ShipmentData(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public final int getQuantity()
+    {
+        return quantity;
+    }
+
+    public String toString()
+    {
+        return "[ShipmentData: quantity=" + quantity + ']';
+    }
+} 
---
+++ tutorialOBDBJE/tutorialOBDBJE/src/Basic/SupplierKey.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basic;
+
+/**
+ *
+ * @author lenovo
+ */
+import java.io.Serializable;
+
+public class SupplierKey implements Serializable
+{
+    private String number;
+
+    public SupplierKey(String number)
+    {
+        this.number = number;
+    }
+
+    public final String getNumber()
+    {
+        return number;
+    }
+
+    public String toString()
+    {
+        return "[SupplierKey: number=" + number + ']';
+    }
+} 
